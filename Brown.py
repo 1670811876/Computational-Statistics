829e7feaa0b68697f5d47e03e0129c0f09296f3c
Alvin Zhu <44566000+1670811876@users.noreply.github.com>
2020-08-31 18:30:20 +0800

    Update Brown.py

--- Brown.py
+++ Brown.py
@@ -1,56 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun  7 14:40:35 2020
-
-@author: 64376
-"""
-
+Created on Sun 6月7日 14:40:35 2020 
+@author: Alvin
+""" 
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif']=['simhei']#用于正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False#用于正常显示负号
+plt.rcParams['font.sans-serif'] = ['simhei']  
+plt.rcParams['axes.unicode_minus'] = False 
 import tushare as ts
 import pandas as pd
-import numpy as np
+import numpy as np 
+ts_code = '600000'
+data_ = ts.get_hist_data(ts_code)  # acquire stock data
+data_ = pd.DataFrame(data_[['close']])  # acquire closing price
+data_.sort_index(inplace=True)  
+data_['ratio'] = [0.0000000000000000000] * len(data_)
+data_['date'] = data_.index
+data_.index = range(data_.shape[0])  # calculate the share price ratio
+for i in range(data_.shape[0] - 1): 
+    data_['ratio'][i+1] = np.log(data_['close'][i+1] / data_['close'][i])  # calculate the elemental parameters
+T = 30  # the cycle is 30 days (one month)
+k = 3  # the first circle
+data_train = data_.iloc[(k-1)*T:(k*T - 1), :]  # select the past month as the training set
+data_test = data_.iloc[(k-1)*T:(k+1)*T - 1, :]  # select the past February as the test set
+mean = data_train['ratio'].sum() / T  # calculate the sample mean
+S_2 = ((data_train['ratio'] - mean)**2).sum()/(T-1)  # calculate the sample variance
+miu = (mean + S_2 / 2) / 1  # stock price drift rate
+sigma = np.sqrt(S_2)  # stock price volatility
 
-#获取股票数据
-ts_code='600000'
-data_=ts.get_hist_data(ts_code)#获得一天数据
+# use geometric Brownian motion model to predict the future period stock price
+S_0 = list(data_train['close'])[-1]  # initial stock price
+data_test['fore_price'] = data_test['close']  # predicted stock price
+for t in range(1, data_train.shape[0] + 1): 
+    Bt = np.random.normal(0, 1, t)[0]  # stochastic value of Brownian motion
+    data_test['fore_price'][t + k * T - 1] = S_0 * np.exp(sigma * Bt + (miu - S_2 / 2) * t)  # predict stock price
 
-data_=pd.DataFrame(data_[['close']])#获得其中的收盘价
-data_.sort_index(inplace=True)#按照日期做升序处理
-data_['ratio']=[0.0000000000000000000]*len(data_)
-data_['date']=data_.index
-data_.index=range(data_.shape[0])
-
-#计算股价比率
-for i in range(data_.shape[0]-1):
-    data_['ratio'][i+1]=np.log(data_['close'][i+1]/data_['close'][i])
-
-#计算基础参数
-T=30#周期为30天（一个月）
-k=3#第一个周期
-data_train=data_.iloc[(k-1)*T:(k*T-1),:]#选取过去一月的时间作为训练集
-data_test=data_.iloc[(k-1)*T:(k+1)*T-1,:]##选取过去第二月的时间作为测试集
-mean=data_train['ratio'].sum()/T#计算样本均值
-S_2=((data_train['ratio']-mean)**2).sum()/(T-1)#计算样本方差
-miu=(mean+S_2/2)/1#股价漂移率
-sigma=np.sqrt(S_2)#股价波动率
-
-#通过几何布朗运动模型预测未来周期股价
-S_0=list(data_train['close'])[-1]#初始股价
-data_test['fore_price']=data_test['close']#预测的股价
-for t in range(1,data_train.shape[0]+1):
-    Bt = np.random.normal(0, 1,t)[0]#布朗运动随机数值
-    data_test['fore_price'][t+k*T-1]=S_0*np.exp(sigma*Bt+(miu-S_2/2)*t)#预测股价
-data_test['fore_price']
-
-#画出预测值和真实值对比图
 days = np.array(data_test['date'], dtype="datetime64[ms]")
-plt.figure(num=k,figsize=(15,8))
-plt.plot(days,data_test['fore_price'],label='预测股价')
-plt.plot(days,data_test['close'],label='实际股价')
-plt.xlabel('日期',fontsize=20)
-plt.ylabel('股价',fontsize=20)
-plt.title("股票“"+ts_code+"”"+str(T)+'天预测值和真实值对比图',fontsize=20)
+plt.figure(num=k, figsize=(15,8))
+plt.plot(days,data_test['fore_price'], label='predicted stock price')
+plt.plot(days,data_test['close'], label='actual stock price')
+plt.xlabel('date', fontsize=20)
+plt.ylabel('price', fontsize=20)
 plt.legend(loc='upper right')
 plt.xticks(rotation=270)
